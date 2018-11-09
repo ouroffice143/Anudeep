720d279acfc395f2987403eb11278e6c792debfd
pallamreddyanudeep <40880859+pallamreddyanudeep@users.noreply.github.com>
2018-11-09 07:24:13 +0530

    new file

---
+++ js/wordcount.js
@@ -0,0 +1,11 @@
+/* words count*/
+var a="hello world how are you now";
+var count=1;
+for(var i=0;i<a.length;i++){
+    if(a[i]==" "){
+        count++;
+    }
+    
+  
+}
+console.log("no of worder "+count);
